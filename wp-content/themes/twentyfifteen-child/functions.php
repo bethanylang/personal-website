f826cd8f52b2ce259ef706cca8068e4c4dd7b5cb
Bethany Lang <bethanylang@gmail.com>
2017-09-10 09:40:58 -0700

    Initial commit

---
+++ wp-content/themes/twentyfifteen-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+function modify_read_more_link() {
+    return '<a class="more-link" href="' . get_permalink() . '">Read more</a>';
+}
+add_filter( 'the_content_more_link', 'modify_read_more_link' );
